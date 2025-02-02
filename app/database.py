868cc2e420a2a9d2cc2a817d700bd6276e097fa2
brandonhlk <brandonhlk@gmail.com>
2025-02-02 21:47:24 +0800

    added models.Base.metadata.create_all(bind=engine)

--- app/database.py
+++ app/database.py
@@ -6,7 +6,9 @@ from .config import settings
 # from psycopg2.extras import RealDictCursor
 # import time
 
-SQLALCHEMY_DATABASE_URL =  f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+# SQLALCHEMY_DATABASE_URL =  f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = 'postgresql://fastapi_mi54_user:nIh7pQz84hoogxOtss0obw7j0OIjc0iG@dpg-cuflellds78s73fm91c0-a.oregon-postgres.render.com/fastapi_mi54'
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
